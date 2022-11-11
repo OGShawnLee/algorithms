b295e8eaea17c1b579e7e77a9686ee130a2f00fd
Shawn Lee <changoloco.evg@gmail.com>
2022-11-10 22:20:12 -0600

    fix(list/v): correct loop end condition

--- src/list/move_zeros_to_the_end.v
+++ src/list/move_zeros_to_the_end.v
@@ -3,8 +3,8 @@ module list
 pub fn move_zeros_to_the_end(mut list []int) []int {
 	for index, mut external in list {
 		if external != 0 { continue }
-		for i := index + 1; index < list.len; i++ {
-			internal := list[i] or { return list }
+		for i := index + 1; i < list.len; i++ {
+			internal := list[i]
 			if internal == 0 { continue } 
 			list[i] = 0
 			external = internal
