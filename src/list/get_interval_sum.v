6a76c30d8f9d20d80786cbf0f66af943ca7a1deb
Shawn Lee <changoloco.evg@gmail.com>
2022-11-13 21:09:47 -0600

    refactor(list/v): use builtin max and min functions

--- src/list/get_interval_sum.v
+++ src/list/get_interval_sum.v
@@ -1,5 +1,7 @@
 module list
 
+import math
+
 pub fn get_interval_sum_recursive(mut intervals [][]int) int {
 	mut total := 0
 	for index, mut interval in intervals {
@@ -10,8 +12,8 @@ pub fn get_interval_sum_recursive(mut intervals [][]int) int {
 			mini := inner_interval[0]
 			maxi := inner_interval[1]
 			if is_overlapped(interval, inner_interval) {
-				interval[0] = min(minimum, mini)
-				interval[1] = max(maximum, maxi)
+				interval[0] = math.min(minimum, mini)
+				interval[1] = math.max(maximum, maxi)
 				intervals.delete(i)
 				return get_interval_sum_recursive(mut intervals)
 			} 
@@ -22,13 +24,5 @@ pub fn get_interval_sum_recursive(mut intervals [][]int) int {
 }
 
 fn is_overlapped(a []int, b []int) bool {
-  return max(a[1], b[1]) - min(a[0], b[0]) < a[1] - a[0] + (b[1] - b[0])
-}
-
-fn max(a int, b int) int {
-	return if a > b { a } else { b }	
-}
-
-fn min(a int, b int) int {
-	return if a > b { b } else { a }
+  return math.max(a[1], b[1]) - math.min(a[0], b[0]) < a[1] - a[0] + (b[1] - b[0])
 }
