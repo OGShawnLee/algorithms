3ebdba70812a22b87f8bce0f78956c76c7f9c14d
Shawn Lee <changoloco.evg@gmail.com>
2022-11-15 14:35:49 -0600

    refactor(aoc): make code more modular

--- src/advent-of-code/day-1/main.py
+++ src/advent-of-code/day-1/main.py
@@ -1,13 +1,19 @@
 from pathlib import Path
 
-file = Path(__file__).with_name("input.txt")
-increased_count = 0
-with file.open("r") as lines:
-  previous_depth = int(lines.readline())
-  for depth in lines:
-    depth = int(depth)
-    if depth > previous_depth:
-        increased_count += 1
-    previous_depth = depth
+file_name = "input.txt"
 
-print(f"times a depth measurement increased from the previous one: {increased_count}")
+def get_depth_increased_amount(file_name: str):
+  file = Path(__file__).with_name(file_name)
+  increased_count = 0
+  with file.open("r") as lines:
+    previous_depth = int(lines.readline())
+    for depth in lines:
+      depth = int(depth)
+      if depth > previous_depth:
+          increased_count += 1
+      previous_depth = depth
+  return increased_count
+
+if __name__ == "__main__":
+  increased_count = get_depth_increased_amount(file_name) 
+  print(f"times a depth measurement increased from the previous one: {increased_count}")
--- src/advent-of-code/day-1/main.v
+++ src/advent-of-code/day-1/main.v
@@ -3,7 +3,12 @@ import os
 const input_path = "./input.txt"
 
 fn main() {
-	lines := get_file_lines(input_path)
+	increased_count := get_depth_increased_amount(input_path)
+	println("times a depth measurement increased from the previous one: $increased_count")
+}
+
+fn get_depth_increased_amount(file_path string) int {
+	lines := get_file_lines(file_path)
 	mut increased_count := 0
 	mut previous_depth := lines[0].int()
 	for line in lines {
@@ -11,7 +16,7 @@ fn main() {
 		if depth > previous_depth { increased_count++ }
 		previous_depth = depth
 	}
-	println("times a depth measurement increased from the previous one: $increased_count")
+	return increased_count
 }
 
 fn get_file_lines(file_path string) []string {
