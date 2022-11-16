ecb0a86a252feb4f627e93d19cf88eb8d0b3875b
Shawn Lee <changoloco.evg@gmail.com>
2022-11-15 22:50:07 -0600

    feat(aoc/v): add day-2 part-2 v solution

--- src/advent-of-code/day-2/main.v
+++ src/advent-of-code/day-2/main.v
@@ -1,8 +1,12 @@
 import os
 
+const file_path = "./input.txt"
+
 fn main() {
-	position := get_submarine_position("./input.txt")
+	mut position := get_submarine_position(file_path)
 	println("submarine position: $position")
+	position = get_submarine_position_compound(file_path) 
+	println("compound submarine position: $position")
 }
 
 fn get_submarine_position(file_path string) int {
@@ -17,6 +21,30 @@ fn get_submarine_position(file_path string) int {
 			"forward" { forward += value }
 			"up" { depth -= value }
 			"down" { depth += value }
+			else {
+				panic("'$direction' is an invalid direction")
+			}
+		}
+	}
+	return forward * depth
+}
+
+fn get_submarine_position_compound(file_path string) int {
+	lines := get_file_lines(file_path)
+	mut aim := 0
+	mut depth := 0
+	mut forward := 0
+	for line in lines {
+		parsed_line := line.split(" ")
+		direction := parsed_line[0]
+		value := parsed_line[1].int()
+		match direction {
+			"forward" {
+				forward += value
+				depth += aim * value
+			}
+			"up" { aim -= value }
+			"down" { aim += value }
 			else {
 				panic("'$direction' is an invalid direction")
 			}
