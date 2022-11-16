aa618fa352a1840fef9af6d5fd1f1a3f283c5b77
Shawn Lee <changoloco.evg@gmail.com>
2022-11-16 13:16:59 -0600

    feat(aoc/ts): add day-1 part-2 ts solution

--- src/advent-of-code/day-1/main.ts
+++ src/advent-of-code/day-1/main.ts
@@ -4,6 +4,8 @@ import { resolve } from "path";
 const filePath = resolve("./input.txt");
 const increasedCount = await getDepthIncreasedAmount(filePath);
 console.log(`times a depth measurement increased from the previous one: ${increasedCount}`);
+const windowIncreasedAmount = await getDepthSlidingWindowIncreasedAmount(filePath, 3);
+console.log(`3-sliding-window depth measurement increased count: ${windowIncreasedAmount}`);
 
 async function getDepthIncreasedAmount(filePath: string) {
   const lines = await getFileLines(filePath);
@@ -13,6 +15,22 @@ async function getDepthIncreasedAmount(filePath: string) {
     const depth = Number(line);
     if (depth > previousDepth) increasedCount += 1;
     previousDepth = depth;
+  }
+  return increasedCount;
+}
+
+async function getDepthSlidingWindowIncreasedAmount(filePath: string, windowLength: number) {
+  const lines = await getFileLines(filePath);
+  let increasedCount = 0;
+  let currentSum = 0;
+  let previousSum = 0;
+  for (let index = 0; index < lines.length; index++) {
+    currentSum += Number(lines[index]);
+    if (index >= windowLength) {
+      currentSum -= Number(lines[index - windowLength]);
+      if (currentSum > previousSum) increasedCount++;
+      previousSum = currentSum;
+    }
   }
   return increasedCount;
 }
