532a84f29778fff654f7386874d2be3740b437e0
Shawn Lee <changoloco.evg@gmail.com>
2022-11-24 12:01:12 -0600

    feat(aoc/py): add day-5 part-2 python solution

--- src/advent-of-code/day-5/main.py
+++ src/advent-of-code/day-5/main.py
@@ -6,6 +6,31 @@ INPUT_FILE_PATH = "./input.txt"
 def create_matrix(size: int):
   return [[0 for i in range(0, size)] for index in range(0, size)]
 
+def get_diagonal_coordinates(start: tuple[int, int], end: tuple[int, int]):
+  minimum_x, maximum_x = minmax(start[0], end[0])
+  minimum_y, maximum_y = minmax(start[1], end[1])
+  is_x_ascending = start[0] < end[0]
+  is_y_ascending = start[1] < end[1]
+  coordinates_len = maximum_x + 1 - minimum_x
+  coordinates: list[list[int]] = [
+    [0, 0] for i in range(coordinates_len)
+  ]
+  for coordinate in coordinates:
+    # i have to do this checks on every iteration cos python :/
+    if is_x_ascending:
+      coordinate[0] = minimum_x
+      minimum_x += 1
+    else:
+      coordinate[0] = maximum_x
+      maximum_x -= 1
+    if is_y_ascending:
+      coordinate[1] = minimum_y
+      minimum_y += 1
+    else:
+      coordinate[1] = maximum_y
+      maximum_y -= 1
+  return coordinates
+
 def get_file_lines(file_path: str):
   file = Path(__file__).with_name(file_path)
   return file.read_text().splitlines()
@@ -35,7 +60,7 @@ def get_overlapped_count(file_path: str, matrix: list[list[int]]):
         tile = row[column_index]
         if tile == 2:
           overlapped_count += 1
-    if is_vertical_line(start, end):
+    elif is_vertical_line(start, end):
       column_index = start[0]
       tail = min(start[1], end[1])
       head = max(start[1], end[1])
@@ -44,6 +69,13 @@ def get_overlapped_count(file_path: str, matrix: list[list[int]]):
         tile = matrix[row_index][column_index]
         if tile == 2:
           overlapped_count += 1
+    else:
+      diagonal_coordinates = get_diagonal_coordinates(start, end)
+      for column_index, row_index in diagonal_coordinates:
+        matrix[row_index][column_index] += 1
+        tile = matrix[row_index][column_index]
+        if tile == 2: 
+          overlapped_count += 1
   return overlapped_count
 
 def is_horizontal_line(start: tuple[int, int], end: tuple[int, int]):
@@ -51,6 +83,9 @@ def is_horizontal_line(start: tuple[int, int], end: tuple[int, int]):
 
 def is_vertical_line(start: tuple[int, int], end: tuple[int, int]):
   return start[0] == end[0]
+
+def minmax(a: int, b: int):
+  return min(a, b), max(a, b)
 
 def print_matrix(matrix: list[list[int]]):
   if len(matrix) < 20:
