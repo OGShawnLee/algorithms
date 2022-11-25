4b4b5fbc18dcfc1b964a6de01875e4fe5f685013
Shawn Lee <changoloco.evg@gmail.com>
2022-11-25 13:27:15 -0600

    feat(aoc/v): add day-1 part-2 v solution

--- src/advent-of-code/2015/day-1/main.v
+++ src/advent-of-code/2015/day-1/main.v
@@ -5,7 +5,21 @@ const input_file_path = "./input.txt"
 fn main() {
 	directions := get_file_lines(input_file_path)!
 	floor_number := get_floor_number_from_directions(directions)
-	println("Floor Number: $floor_number")
+	position := get_basement_direction_position(directions)!
+	println("Floor Number: $floor_number | Basement Direction Position: $position")
+}
+
+fn get_basement_direction_position(lines []string) !int {
+	mut floor_number := 0
+	for line in lines {
+		for index, direction_code in line {
+			floor_number += get_direction_code_value(direction_code)
+			if floor_number == -1 {
+				return index + 1
+			}
+		}
+	}
+	panic("Unable to Find Basement Direction Position")
 }
 
 fn get_direction_code_value(code u8) int {
