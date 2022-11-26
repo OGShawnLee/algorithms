da27907042183f90001ae55e411f4dcf4b22b448
Shawn Lee <changoloco.evg@gmail.com>
2022-11-26 15:48:24 -0600

    feat(aoc/rb): add day-1 part-2 ruby solution

--- src/advent-of-code/2021/day-1/main.rb
+++ src/advent-of-code/2021/day-1/main.rb
@@ -2,6 +2,24 @@ def get_file_lines file_path
   IO.readlines(file_path, chomp: true)
 end
 
+def get_depth_increased_amount_sliding_window lines, window_length
+  increased_count = 0
+  current_count = 0
+  previous_count = 0
+  lines.each.with_index do |depth, index|
+    depth = depth.to_i
+    current_count += depth
+    if index >= window_length
+      current_count -= lines[index - window_length].to_i
+      if current_count > previous_count
+        increased_count += 1
+      end
+      previous_count = current_count
+    end
+  end
+  increased_count
+end
+
 def get_depth_increased_amount lines
   count = 0
   previous_depth = lines[0].to_i
@@ -16,7 +34,10 @@ def get_depth_increased_amount lines
 end
 
 INPUT_FILE_PATH = "./input.txt"
+SLIDING_WINDOW_LENGTH = 3
 
 lines = get_file_lines INPUT_FILE_PATH
 increased_count = get_depth_increased_amount lines
 puts "times a depth measurement increased from the previous one: #{increased_count}"
+window_increased_count = get_depth_increased_amount_sliding_window lines, SLIDING_WINDOW_LENGTH
+puts "#{SLIDING_WINDOW_LENGTH}-sliding-window depth measurement increased count: #{window_increased_count}"
