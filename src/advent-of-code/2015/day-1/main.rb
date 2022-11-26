8a7b8109ce90b4b768e60b744a7871058fbc6a93
Shawn Lee <changoloco.evg@gmail.com>
2022-11-26 14:34:30 -0600

    feat(aoc/rb): add day-1 part-2 ruby solution

--- src/advent-of-code/2015/day-1/main.rb
+++ src/advent-of-code/2015/day-1/main.rb
@@ -1,3 +1,16 @@
+def get_basement_direction_position lines
+  floor_number = 0
+  for line in lines
+    line.split("").each.with_index do |char, index|
+      floor_number += get_direction_char_value char
+      if floor_number == -1
+        return index + 1
+      end
+    end
+  end
+  raise "Unable to Find Basement Direction Position"
+end
+
 def get_file_lines file_path
   IO.readlines(file_path, chomp: true)
 end
@@ -20,4 +33,5 @@ INPUT_FILE_NAME = "./input.txt"
 
 directions = get_file_lines INPUT_FILE_NAME
 floor_number = get_floor_number_from_directions directions
-puts "Floor Number: #{floor_number}"
+position = get_basement_direction_position directions
+puts "Floor Number: #{floor_number} | Basement Direction Position: #{position}"
