ed089cf634fb14c9d38e4334dba7dd5b7daa26f6
Shawn Lee <changoloco.evg@gmail.com>
2022-11-26 15:25:25 -0600

    feat(aoc/rb): add day-2 part-2 ruby solution

--- src/advent-of-code/2015/day-2/main.rb
+++ src/advent-of-code/2015/day-2/main.rb
@@ -1,3 +1,9 @@
+def calculate_box_ribbon length, width, height
+  ribbon = length * 2 + width * 2
+  bow = length * width * height
+  ribbon + bow
+end
+
 def calculate_box_wrapping_paper length, width, height
   (2 * length * width) + (2 * width * height) + (2 * height * length) + length * width
 end
@@ -7,12 +13,14 @@ def get_file_lines file_path
 end
 
 def get_total_wrapping_paper lines
+  ribbon = 0
   paper = 0
   for line in lines
     length, width, height = parse_present_dimensions line
     paper += calculate_box_wrapping_paper length, width, height
+    ribbon += calculate_box_ribbon length, width, height
   end
-  paper
+  return paper, ribbon
 end 
 
 def parse_present_dimensions line
@@ -25,5 +33,5 @@ end
 INPUT_FILE_NAME = "./input.txt"
 
 dimensions = get_file_lines INPUT_FILE_NAME
-paper = get_total_wrapping_paper dimensions
-puts "Total Wrapping Paper: #{paper} square feet"
+paper, ribbon = get_total_wrapping_paper dimensions
+puts "Total Wrapping Paper: #{paper} square feet | Total Ribbon: #{ribbon} feet"
