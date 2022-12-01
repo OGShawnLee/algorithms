c49d25202ce3951cc19db00614d53e5f1638934f
Shawn Lee <changoloco.evg@gmail.com>
2022-11-30 22:10:12 -0600

    feat(aoc/py): add day-6 part-2 python solution

--- src/advent-of-code/2015/day-6/main.py
+++ src/advent-of-code/2015/day-6/main.py
@@ -1,9 +1,9 @@
 from pathlib import Path
 from re import findall, search
 
-def create_matrix(size: int):
+def create_matrix(size: int, value: int):
   return [
-    [-1 for _ in range(0, size)]
+    [value for _ in range(0, size)]
     for _ in range(0, size)
   ]
 
@@ -11,8 +11,16 @@ def get_file_lines(file_path: str):
   file = Path(__file__).with_name(file_path)
   return file.read_text().splitlines()
 
+def get_brightness_light_count(file_path: str, matrix_size: int):
+  lines = get_file_lines(file_path)
+  matrix = create_matrix(matrix_size, 0)
+  count = 0
+  for line in lines:
+    count += get_instruction_brightness_count(line, matrix)
+  return count
+
 def get_lit_lights_count(file_path: str, matrix_size: int):
-  matrix = create_matrix(matrix_size)
+  matrix = create_matrix(matrix_size, -1)
   count = 0
   lines = get_file_lines(file_path)
   for line in lines:
@@ -20,36 +28,50 @@ def get_lit_lights_count(file_path: str, matrix_size: int):
   return count
 
 def get_instruction_light_count_change(line: str, matrix: list[list[int]]):
-  instruction, start, end = parse_line(line)
-  change = 0
-  if instruction == "toggle":
-    change += use_instruction_light_count_change(start, end, lambda r_idx, c_idx: on_toggle(matrix, r_idx, c_idx))
-  elif instruction == "turn on":
-    change += use_instruction_light_count_change(start, end, lambda r_idx, c_idx: on_turn_on(matrix, r_idx, c_idx))
-  elif instruction == "turn off":
-    change += use_instruction_light_count_change(start, end, lambda r_idx, c_idx: on_turn_off(matrix, r_idx, c_idx))
-  else:
-    raise ValueError(f"Invalid Instruction: {instruction}")
-  return change
+  return use_instruction_matching(line, matrix, on_toggle, on_turn_on, on_turn_off)
+
+def get_instruction_brightness_count(line: str, matrix: list[list[int]]):
+  return use_instruction_matching(
+    line=line,
+    matrix=matrix,
+    on_toggle=on_toggle_brightness,
+    on_turn_on=on_turn_on_brightness,
+    on_turn_off=on_turn_off_brightness
+  )
 
 def on_toggle(matrix: list[list[int]], row_index: int, column_index: int):
   light_state = matrix[row_index][column_index]
   matrix[row_index][column_index] = 1 if light_state == -1 else -1 
   return matrix[row_index][column_index]
-  
+
+def on_toggle_brightness(matrix: list[list[int]], row_index: int, column_index: int):
+  matrix[row_index][column_index] += 2
+  return 2
+
 def on_turn_on(matrix: list[list[int]], row_index: int, column_index: int):
   light_state = matrix[row_index][column_index]
   if light_state == 1:
     return 0
   matrix[row_index][column_index] = 1  
   return matrix[row_index][column_index]
-  
+
+def on_turn_on_brightness(matrix: list[list[int]], row_index: int, column_index: int):
+  matrix[row_index][column_index] += 1  
+  return 1
+
 def on_turn_off(matrix: list[list[int]], row_index: int, column_index: int):
   light_state = matrix[row_index][column_index]
   if light_state == -1:
     return 0
   matrix[row_index][column_index] = -1  
   return matrix[row_index][column_index]
+
+def on_turn_off_brightness(matrix: list[list[int]], row_index: int, column_index: int):
+  brightness = matrix[row_index][column_index]
+  if brightness == 0:
+    return 0
+  matrix[row_index][column_index] -= 1
+  return -1  
 
 def parse_line(line: str):
   instruction_match = search("(\w+ (off|on))|(\w+)", line)
@@ -65,11 +87,24 @@ def parse_line(line: str):
   ]
   return instruction, start, end
 
-def use_instruction_light_count_change(start: list[int], end: list[int], fn):
+def use_instruction_light_count_change(start: list[int], end: list[int], matrix: list[list[int]], fn):
   change = 0
   for row_index in range(start[0], end[0] + 1):
     for column_index in range(start[1], end[1] + 1):
-      change += fn(row_index, column_index)
+      change += fn(matrix, row_index, column_index)
+  return change
+
+def use_instruction_matching(line: str, matrix: list[list[int]], on_toggle, on_turn_on, on_turn_off):
+  instruction, start, end = parse_line(line)
+  change = 0
+  if instruction == "toggle":
+    change += use_instruction_light_count_change(start, end, matrix, on_toggle)
+  elif instruction == "turn on":
+    change += use_instruction_light_count_change(start, end, matrix, on_turn_on)
+  elif instruction == "turn off":
+    change += use_instruction_light_count_change(start, end, matrix, on_turn_off)
+  else:
+    raise ValueError(f"Invalid Instruction: {instruction}")
   return change
 
 INPUT_FILE_NAME = "./input.txt"
@@ -78,3 +113,5 @@ MATRIX_SIZE = 1000
 if __name__ == "__main__":
   count = get_lit_lights_count(INPUT_FILE_NAME, MATRIX_SIZE)
   print(f"Lit Lights Count: {count}")
+  brightness = get_brightness_light_count(INPUT_FILE_NAME, MATRIX_SIZE)
+  print(f"Light Brightness: {brightness}")
