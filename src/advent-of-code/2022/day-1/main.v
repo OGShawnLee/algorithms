2963c462dddf5215fb62f5f040e000cb3318cc1a
Shawn Lee <changoloco.evg@gmail.com>
2022-12-01 15:17:37 -0600

    feat(aoc/v): add day-1 part-2 v solution

--- src/advent-of-code/2022/day-1/main.v
+++ src/advent-of-code/2022/day-1/main.v
@@ -4,14 +4,44 @@ import os { read_lines }
 const input_path = "./input.txt"
 
 fn main() {
-	max_calories := find_max_elf_calories(input_path)!
+	lines := get_file_lines(input_path)!
+	max_calories_future := spawn find_max_elf_calories(lines)
+	top_3_future := spawn find_top_3_max_elf_calories(lines)
+	max_calories := max_calories_future.wait()
 	println("Maximum Elf Calories: $max_calories")
+	first, second, third := top_3_future.wait()
+	total := first + second + third
+	println("Top 3 Maximum Elf Calories: $first, $second, $third | Total: $total")
 }
 
-fn find_max_elf_calories(file_path string) !int  {
-	lines := get_file_lines(file_path) or {
-		panic("unable to find maximum elf calories -> $err")
+fn insert_calories(current int, mut calories []int) {
+	if current > calories.first() {
+		calories.prepend(current)
+		calories.delete_last()
+	} else if current > calories[1] {
+		calories.insert(1, current)
+		calories.delete_last()
+	} else if current > calories.last() {
+		calories[calories.len - 1] = current
 	}
+}
+
+fn find_top_3_max_elf_calories(lines []string) (int, int, int) {
+	mut calories := []int { cap: 3, len: 3, init: 0 }
+	mut current := 0
+	for line in lines {
+		if line == "" {
+			insert_calories(current, mut calories)
+			current = 0
+		} else {
+			current += line.int()
+		}
+	}
+	insert_calories(current, mut calories)
+	return calories[0], calories[1], calories[2]
+}
+
+fn find_max_elf_calories(lines []string) int {
 	mut current_elf_calories := 0
 	mut max_elf_calories := 0
 	for line in lines {
