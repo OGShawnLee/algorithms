9f2972ac3db1e9fb830c66a1f64c52710af72630
Shawn Lee <changoloco.evg@gmail.com>
2022-12-03 21:53:46 -0600

    feat(aoc/v): add day-3 part-2 v solution

--- src/advent-of-code/2022/day-3/main.v
+++ src/advent-of-code/2022/day-3/main.v
@@ -8,12 +8,48 @@ const (
 )
 
 fn main() {
-	input_count_thread := go get_items_priority_sum(input_path)
-	example_count_thread := go get_items_priority_sum(example_path)
+	input_lines := get_file_lines(input_path)!
+	example_lines := get_file_lines(example_path)!
+	input_count_thread := go get_items_priority_sum(input_lines)
+	example_count_thread := go get_items_priority_sum(example_lines)
 	mut count := example_count_thread.wait()!
 	println("Example Prority Sum: $count")
 	count = input_count_thread.wait()!
 	println("Input Prority Sum: $count")
+	input_priority_count := go get_common_group_items_priority_count(input_lines)
+	example_priority_count := go get_common_group_items_priority_count(example_lines)
+	mut priority_count := example_priority_count.wait()!
+	println("Example Prority Sum: $priority_count")
+	priority_count = input_priority_count.wait()!
+	println("Input Prority Sum: $priority_count")
+}
+
+fn get_common_group_items_priority_count(lines []string) !int {
+	mut current_group := []string { cap: 3 }
+	mut priority_count := 0
+	for line in lines {
+		if current_group.len != 3 {
+			current_group << line
+		}
+		if current_group.len == 3 {
+			common_char := find_common_group_item_char(current_group)!
+			priority := get_char_priority(common_char)!
+			priority_count += priority
+			current_group.clear()
+		}
+	}
+	return priority_count
+}
+
+fn find_common_group_item_char(group []string) !u8 {
+	for index, code in letters {
+		upper_code := uppercase_letters[index]
+		is_common_upper_char := group.all(it.index_u8(upper_code) != -1)
+		if is_common_upper_char { return upper_code }
+		is_common_char := group.all(it.index_u8(code) != -1)
+		if is_common_char { return code }
+	}
+	panic("unable to find common group item char in $group")
 }
 
 fn find_repeated_char(line string) !u8 {
@@ -45,9 +81,8 @@ fn get_file_lines(file_path string) ![]string {
 	return lines
 }
 
-fn get_items_priority_sum(file_path string) !int {
+fn get_items_priority_sum(lines []string) !int {
 	mut count := 0
-	lines := get_file_lines(file_path)!
 	for line in lines {
 		code := find_repeated_char(line)!
 		priority := get_char_priority(code)!
