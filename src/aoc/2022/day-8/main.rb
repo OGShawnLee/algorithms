06e30de43c445bcf057e97f3415eb337bdfc85ba
Shawn Lee <changoloco.evg@gmail.com>
2022-12-16 20:52:23 -0600

    feat(aoc/rb): add day-8 part-2 ruby solution

--- src/aoc/2022/day-8/main.rb
+++ src/aoc/2022/day-8/main.rb
@@ -16,6 +16,33 @@ def get_file_lines file_path
   IO.readlines(file_path, chomp: true)
 end
 
+def get_max_tree_scenic_score lines, size
+  max = 0
+  grid = create_tree_grid(lines, size)
+  grid.each.with_index do |row, row_index|
+    row.each.with_index do |tree_height, column_index|
+      scenic_score = get_tree_scenic_score(grid, row_index, column_index, tree_height)
+      max = scenic_score if scenic_score > max
+    end
+  end
+  return max
+end
+
+
+def get_tree_scenic_score grid, row_index, column_index, tree_height
+  scores = DIRECTIONS.map do |direction|
+    count = 0
+    use_direction_loop(grid, row_index, column_index, tree_height, direction, count) do |height|
+      count += 1
+      if tree_height <= height
+        true 
+      end
+    end
+    count
+  end
+  return scores.reduce(:*)
+end
+  
 def get_visible_tree_count lines, size
   count = 0
   grid = create_tree_grid(lines, size)
@@ -43,37 +70,51 @@ def is_visible grid, row_index, column_index, tree_height
 end 
 
 def is_visible_from grid, row_index, column_index, tree_height, direction
+  use_direction_loop(grid, row_index, column_index, tree_height, direction, true) do |height|
+    if height >= tree_height
+      [true, false]
+    end
+  end 
+end
+
+def print_results file_name, count, scenic_score
+  puts "#{file_name} File Results:"
+  puts "Visible Trees Count: #{count}"
+  puts "Maximum Tree Scenic Score: #{scenic_score}"
+end
+
+def use_direction_loop grid, row_index, column_index, tree_height, direction, initial_value
   len = grid.length
   case direction
   when "top"
     (row_index - 1).downto(0) do |r_idx|
       height = grid[r_idx][column_index]
-      return false if height >= tree_height 
+      return_early, value = yield height
+      return value if return_early
     end
   when "right"
     (column_index + 1).upto(len - 1) do |c_idx|
       height = grid[row_index][c_idx]
-      return false if height >= tree_height 
+      return_early, value = yield height
+      return value if return_early
     end
   when "bottom"
     (row_index + 1).upto(len - 1) do |r_idx|
       height = grid[r_idx][column_index]
-      return false if height >= tree_height
+      return_early, value = yield height
+      return value if return_early
     end
   when "left"
     (column_index - 1).downto(0) do |c_idx|
       height = grid[row_index][c_idx]
-      return false if height >= tree_height
+      return_early, value = yield height
+      return value if return_early
     end
   end
-  return true
+  return initial_value
 end
 
-def print_results file_name, count
-  puts "#{file_name} File Results:"
-  puts "Visible Trees Count: #{count}"
-end
-
+DIRECTIONS = ["top", "right", "bottom", "left"]
 FILE_PATH_EXAMPLE = "./example.txt"
 FILE_PATH_INPUT = "./input.txt"
 GRID_SIZE_EXAMPLE = 5
@@ -81,8 +122,10 @@ GRID_SIZE_INPUT = 99
 
 lines = get_file_lines(FILE_PATH_EXAMPLE)
 count = get_visible_tree_count(lines, GRID_SIZE_EXAMPLE)
-print_results("Example", count)
+scenic_score = get_max_tree_scenic_score(lines, GRID_SIZE_EXAMPLE)
+print_results("Example", count, scenic_score)
 
 lines = get_file_lines(FILE_PATH_INPUT)
 count = get_visible_tree_count(lines, GRID_SIZE_INPUT)
-print_results("Input", count)
+scenic_score = get_max_tree_scenic_score(lines, GRID_SIZE_INPUT)
+print_results("Input", count, scenic_score)
