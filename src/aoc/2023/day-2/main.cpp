98c6ead9b4bd6d37827311202dc14d1d02c13b94
Shawn <changoloco.evg@gmail.com>
2023-12-31 18:54:23 +0000

    feat(aoc/c++): add 2023 day-2 part-1 soolution

---
+++ src/aoc/2023/day-2/main.cpp
@@ -0,0 +1,129 @@
+#include <fstream>
+#include <functional>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+struct Cube {
+  string name;
+  int amount;
+};
+
+struct Game {
+  int id;
+  vector<vector<Cube>> sets;
+};
+
+void each_line(string file_path, function<void(string)> fn) {
+  ifstream file = ifstream(file_path);
+  string line;
+  while (getline(file, line)) {
+    fn(line);
+  }
+}
+
+// we do parsing right away; we assume the input is always valid
+Game parse(string line) {
+  Game game;
+  string slice;
+  bool is_pending = true;
+  bool is_missing_cube_quantity = true;
+
+  for (int index = 0; index < line.length(); index++) {
+    char token = line[index];
+
+    if (token == ' ' && slice.empty() == false) {
+      if (is_pending) {
+        if (slice == "Game") slice = "";
+        continue;
+      }
+
+      if (is_missing_cube_quantity) {
+        Cube cube;
+        cube.amount = stoi(slice);
+        game.sets.back().push_back(cube);
+        is_missing_cube_quantity = false;
+        slice = "";
+      }
+
+      continue;
+    }
+
+    // start-token
+    if (token == ':') {
+      // adding first set
+      vector<Cube> initial_set;
+      game.sets.push_back(initial_set);
+
+      // setting game id
+      game.id = stoi(slice);
+      is_pending = false;
+      slice = "";
+      continue;
+    }
+
+    // end-set-token
+    if (token == ',') {
+      // adding cube name
+      Cube &current_cube = game.sets.back().back();
+      current_cube.name = slice;
+      is_missing_cube_quantity = true;
+      slice = "";
+      continue;
+    }
+
+    // end-set-token 
+    if (token == ';') {
+      // adding current set last cube name
+      Cube &current_cube = game.sets.back().back();
+      current_cube.name = slice;
+
+      // adding new set
+      vector<Cube> set;
+      game.sets.push_back(set);
+      is_missing_cube_quantity = true;
+      slice = "";
+      continue;
+    }
+
+    slice += token;
+  }
+
+  // each statement ends with a cube name
+  Cube &final_cube = game.sets.back().back();
+  final_cube.name = slice;
+
+  return game;
+}
+
+int main() {
+  int id_total = 0;
+
+  each_line("input.txt", [&](string line) {
+    Game game = parse(line);
+    bool is_possible = true;
+
+    for (vector<Cube> set : game.sets) {
+      for (Cube cube : set) {
+        if (cube.name == "red") {
+          if (cube.amount > 12) is_possible = false;
+        }
+
+        if (cube.name == "green") {
+          if (cube.amount > 13) is_possible = false;
+        }
+
+        if (cube.name == "blue") {
+          if (cube.amount > 14) is_possible = false;
+        }
+      }
+    }
+
+    if (is_possible) id_total += game.id;
+  });
+
+  printf("ID Total: %d\n", id_total);
+
+  return 0;
+}
